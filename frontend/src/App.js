dab00401da891cd50497232810fe5bcac2007eaa
Aman Singh <94719475+AmanSingh470@users.noreply.github.com>
2025-03-20 16:17:17 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import Navbar from './components/Navbar.js';
 import Home from './components/Home.js';
+import AboutDeveloper from './components/AboutDeveloper.js';
 import Footer from './components/Footer.js';
 import {
   BrowserRouter as Router,
@@ -17,6 +18,7 @@ function App() {
       <Navbar />
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<AboutDeveloper />}/>
           <Route path="/topheadlines" element={<News key="topheadlines" category="topheadlines"/>}/>
           <Route path="/sports" element={<News key="sports" category="sports"/>}/>
           <Route path="/entertainment" element={<News key="entertainment" category="entertainment"/>}/>
